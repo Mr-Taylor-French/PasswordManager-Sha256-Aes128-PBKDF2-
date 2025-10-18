bc0c0eea34c4ad0353bb659426cbb375c02dc3eb
Mr-Taylor-French <jacen.taylor12@gmail.com>
2025-10-17 19:37:02 -0600

    Finally completed the compression part of sha256, just need to finish digest part

--- SHA256.h
+++ SHA256.h
@@ -14,7 +14,7 @@ private:
     std::vector<std::vector<uint8_t>> blocks; // Each block should be 512-bits (64 bytes), block vector has elements of 1-byte(8bit), there should be 64 elements of the inner vector making 64bytes total in inner
 
     //even though values are derived by fancy math functions, same for round constants btw, values are still used as 32 bit integers
-    std::vector<uint32_t> hash_constants = { 
+    std::vector<uint32_t> hash_constants = {//hash constants get mutated at the end of each compression cycle, so constant is misleading, hash_value or _data is also though, sticking to doc. names 
         0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
     }; //each hash constant is 32 bits, theres 8 * 4bytes so 32bytes total space 
     std::vector<uint32_t> round_constants = {
@@ -50,19 +50,18 @@ public:
         }//after this loop, msg_bytes will be multiple of 64 bytes in length, if short enough, just 64bytes
 
         // Split into 512-bit (64-byte) blocks
-	int counter = 0;
         for (size_t i = 0; i < msg_bytes.size(); i += 64) {//step by 64 as each chunk being process is 64 bytes
             std::vector<uint8_t> block(msg_bytes.begin() + i, msg_bytes.begin() + i + 64); //create a block, each block is 1byte   
             blocks.push_back(block); //push that block into last position of blocks vector, blocks should have multiple of 64 bytes, 1 byte blocks
-	  
-	    
-        }// after this, the message is fully divided into 512-bit (64-byte) blocks stored in 'blocks', length of message determines how many blocks, if short only one 512bit block is used
-	message_schedule(blocks[0]);
 
+	    if (block.size() != 64){
+		std::cerr << "Error: length of block is " << block.size() << ", thats bad fuckface, should be 64\n";
+		std::exit(EXIT_FAILURE);
+	    }
+        }// after this, the message is fully divided into 512-bit (64-byte) blocks stored in 'blocks', length of message determines how many blocks, if short only one 512bit block is used
     }
 
-    //std::vector<uint32_t>
-    void message_schedule(const std::vector<uint8_t>& block){ 
+    std::vector<uint32_t> message_schedule(const std::vector<uint8_t>& block){ 
         //this will process one block of blocks, and create schedule for compression of that one 64 byte block, the other blocks if there are any, will use their own message schedule and compression,
         //take block and put them in vector of 32 bit words/elements, 4 block elements(uint8_t) to each word, block being 1 byte, 
         
@@ -95,10 +94,52 @@ public:
 	    uint32_t word = message_words[i-16] + s0 + message_words[i-7] + s1; //c++ already handles the mod 2^32 wrapping if integers overflow so no need to worry about that,
 	    message_words.push_back(word);
 	}
+	if (message_words.size() != 64){
+	    std::cerr << "Error: length of message_schedule vector is " << message_words.size() << ", thats bad fuckface, should be 64\n";
+	    std::exit(EXIT_FAILURE);
+	}
+	return message_words;
+    }
 
+
+    void compression(const std::vector<uint8_t>& block){
+	uint32_t a = hash_constants[0];
+	uint32_t b = hash_constants[1];
+	uint32_t c = hash_constants[2];
+	uint32_t d = hash_constants[3];
+	uint32_t e = hash_constants[4];
+	uint32_t f = hash_constants[5];
+	uint32_t g = hash_constants[6];
+	uint32_t h = hash_constants[7];
 	
-
-	//return message_words;
+	std::vector<uint32_t> message_schedy = message_schedule(block);
+	
+	uint32_t S1, ch, temp1, S0, maj, temp2;
+	for (size_t i = 0; i < 64; i++){
+	    S1 = ((e >> 6) | (e << 26)) ^ ((e >> 11) | (e << 21)) ^ ((e >> 25) | (e << 7)); //S1 = three right rotations and xor'ed together
+	    ch = (e & f) ^ ((~e) & g);
+	    temp1 = h + S1 + ch + round_constants[i] + message_schedy[i]; //again, mod 2^32 addition is handled by c++
+	    S0 = ((a >> 2) | (a << 30)) ^ ((a >> 13) | (a << 19)) ^ ((a >> 22) | (a << 10)); //S0 = three right rotations and xor'ed together, capitals to distinguish from last s0, s1
+	    maj = (a & b) ^ (a & c) ^ (b & c);
+	    temp2 = S0 + maj;
+	    h = g;
+	    g = f;
+	    f = e;
+	    e = d + temp1;
+	    d = c;
+	    c = b;
+	    b = a;
+	    a = temp1 + temp2;
+	}
+	//now just modify existing hash constants
+	hash_constants[0] = hash_constants[0] + a;
+	hash_constants[1] = hash_constants[1] + b;
+	hash_constants[2] = hash_constants[2] + c;
+        hash_constants[3] = hash_constants[3] + d;
+	hash_constants[4] = hash_constants[4] + e;
+        hash_constants[5] = hash_constants[5] + f;
+	hash_constants[6] = hash_constants[6] + g;
+	hash_constants[7] = hash_constants[7] + h;
     }
 
     
